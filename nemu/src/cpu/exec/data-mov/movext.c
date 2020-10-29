7236f37fbfcd32eb34d9bedd007254a765970629
KuangjuX <18630816527@163.com>
2020-10-29 21:17:00 +0800

    add ins

--- nemu/src/cpu/exec/data-mov/movext.c
+++ nemu/src/cpu/exec/data-mov/movext.c
@@ -1,39 +1,14 @@
-#include "cpu/exec/template-start.h"
+#include "cpu/exec/helper.h"
 
-#if DATA_BYTE == 2 || DATA_BYTE  == 4
-make_helper(concat(movzb_, SUFFIX)) {
-	int len = decode_rm2r_b(eip + 1);
-	REG(op_dest->reg) = op_src->val;
+#define DATA_BYTE 2
+#include "movext-template.h"
+#undef DATA_BYTE
 
-	print_asm("movzb" str(SUFFIX) " %s,%%%s", op_src->str, REG_NAME(op_dest->reg));
-	return len + 1;
-}
+#define DATA_BYTE 4
+#include "movext-template.h"
+#undef DATA_BYTE
 
-make_helper(concat(movsb_, SUFFIX)) {
-	int len = decode_rm2r_b(eip + 1);
-	REG(op_dest->reg) = (int8_t)op_src->val;
+/* for instruction encoding overloading */
 
-	print_asm("movsb" str(SUFFIX) " %s,%%%s", op_src->str, REG_NAME(op_dest->reg));
-	return len + 1;
-}
-#endif
-
-#if DATA_BYTE  == 4
-make_helper(concat(movzw_, SUFFIX)) {
-	int len = decode_rm2r_w(eip + 1);
-	REG(op_dest->reg) = op_src->val;
-
-	print_asm("movzw" str(SUFFIX) " %s,%%%s", op_src->str, REG_NAME(op_dest->reg));
-	return len + 1;
-}
-
-make_helper(concat(movsw_, SUFFIX)) {
-	int len = decode_rm2r_w(eip + 1);
-	REG(op_dest->reg) = (int16_t)op_src->val;
-
-	print_asm("movsw" str(SUFFIX) " %s,%%%s", op_src->str, REG_NAME(op_dest->reg));
-	return len + 1;
-}
-#endif
-
-#include "cpu/exec/template-end.h"
+make_helper_v(movzb)
+make_helper_v(movsb)
