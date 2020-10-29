970dbb3f915df6a9458a1d5053c8cd19a243d4d4
KuangjuX <18630816527@163.com>
2020-10-29 16:40:24 +0800

    test

--- nemu/src/cpu/exec/lods/lods-template.h
+++ nemu/src/cpu/exec/lods/lods-template.h
@@ -3,12 +3,18 @@
 #define instr lods
 
 make_helper(concat(lods_, SUFFIX)) {
-	REG(R_EAX) = swaddr_read(reg_l(R_ESI), DATA_BYTE);
-	if (cpu.DF == 0) reg_l (R_ESI) += DATA_BYTE;
-	else reg_l (R_ESI) -= DATA_BYTE;
+	// REG(R_EAX) = swaddr_read(reg_l(R_ESI), DATA_BYTE);
+	// if (cpu.DF == 0) reg_l (R_ESI) += DATA_BYTE;
+	// else reg_l (R_ESI) -= DATA_BYTE;
 	
-	print_asm("loads%s", str(SUFFIX));
-	return 1;
+	// print_asm("loads%s", str(SUFFIX));
+	// return 1;
+
+	REG(R_EAX) = MEM_R(reg_l(R_ESI));
+    if (cpu.DF == 0) reg_l(R_ESI) += DATA_BYTE;
+    else reg_l(R_ESI) -= DATA_BYTE;
+	print_asm_template2();
+    return 1;
 }
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/scas/scas-template.h
+++ nemu/src/cpu/exec/scas/scas-template.h
@@ -3,17 +3,40 @@
 #define instr scas
 
 make_helper(concat(scas_, SUFFIX)) {
-	swaddr_t s1 = REG(R_EAX), s2 = swaddr_read(reg_l(R_EDI), DATA_BYTE);
-	uint32_t res = s1 - s2;
-	if (cpu.DF == 0) reg_l(R_EDI) += DATA_BYTE;
-	else reg_l(R_EDI) -= DATA_BYTE;
-	concat(updateCPU_, SUFFIX) (res);
-	int len = (DATA_BYTE << 3) - 1;
-	cpu.CF = s1 < s2;
-    cpu.OF = ((s1 >> len) != (s2 >> len) && (s2 >> len) == cpu.SF);
-	print_asm("scas%s", str(SUFFIX));
+	// swaddr_t s1 = REG(R_EAX), s2 = swaddr_read(reg_l(R_EDI), DATA_BYTE);
+	// uint32_t res = s1 - s2;
+	// if (cpu.DF == 0) reg_l(R_EDI) += DATA_BYTE;
+	// else reg_l(R_EDI) -= DATA_BYTE;
+	// concat(updateCPU_, SUFFIX) (res);
+	// int len = (DATA_BYTE << 3) - 1;
+	// cpu.CF = s1 < s2;
+    // cpu.OF = ((s1 >> len) != (s2 >> len) && (s2 >> len) == cpu.SF);
+	// print_asm("scas%s", str(SUFFIX));
 
-	return 1;
+	// return 1;
+
+	uint32_t t2 = MEM_R(reg_l(R_EDI));
+    uint32_t t1 = REG(R_EAX);
+
+    uint32_t ret = t1 - t2;
+
+    if (cpu.DF == 0) reg_l(R_EDI) += DATA_BYTE;
+    else reg_l(R_EDI) -= DATA_BYTE;
+	/* TODO: Update EFLAGS. */
+    cpu.ZF = !ret;
+    cpu.SF = ret >> ((DATA_BYTE << 3) - 1);
+    cpu.CF = (t1 < t2);
+    int tmp1 = (t1) >> ((DATA_BYTE << 3) - 1);
+    int tmp2 = (t2) >> ((DATA_BYTE << 3) - 1);
+    cpu.OF = (tmp1 != tmp2 && tmp2 == cpu.SF);
+    ret ^= ret >> 4;
+    ret ^= ret >> 2;
+    ret ^= ret >> 1;
+    ret &= 1;
+    cpu.PF = !ret;
+
+	print_asm("scas");
+    return 1;
 }
 
 #include "cpu/exec/template-end.h"
