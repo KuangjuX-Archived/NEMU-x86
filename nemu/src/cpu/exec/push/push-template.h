52be6cb2d9f32437695e2026c8912de3e866694a
KuangjuX <18630816527@163.com>
2020-10-30 13:13:31 +0800

    modify some

--- nemu/src/cpu/exec/push/push-template.h
+++ nemu/src/cpu/exec/push/push-template.h
@@ -13,10 +13,10 @@ void do_execute() {
 	}
 	print_asm_template1();
 }
+make_instr_helper(i)
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 make_instr_helper(r)
 make_instr_helper(rm)
 #endif
-make_instr_helper(i)
 
 #include "cpu/exec/template-end.h"
