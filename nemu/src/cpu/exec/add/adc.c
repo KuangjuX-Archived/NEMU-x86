8e8b8ab200bb848a6c3684008357d19f378fd681
KuangjuX <18630816527@163.com>
2020-10-30 13:38:50 +0800

    add some

--- nemu/src/cpu/exec/add/adc.c
+++ nemu/src/cpu/exec/add/adc.c
@@ -12,8 +12,10 @@
 #include "adc-template.h"
 #undef DATA_BYTE
 
-make_helper_v(adc_i2rm)
-make_helper_v(adc_si2rm)
-make_helper_v(adc_r2rm)
-make_helper_v(adc_rm2r)
-make_helper_v(adc_i2a)
+/* for instruction encoding overloading */
+
+make_helper_v(adc_i2a);
+make_helper_v(adc_i2rm);
+make_helper_v(adc_si2rm);
+make_helper_v(adc_r2rm);
+make_helper_v(adc_rm2r);
