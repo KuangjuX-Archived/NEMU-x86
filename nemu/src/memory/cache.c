ac8ad17a3394d6492bd26f59e8a48bb31d074035
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-08 17:17:09 +0800

    >  compile NEMU 3019244189 root Linux qcx-virtual-machine 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:17:09 up  1:51,  1 user,  load average: 0.28, 0.34, 0.27 38dde7f755b145a4c223287ff3b841b8a40c1074

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -1,5 +1,9 @@
-#include "memory/cache.h"
 #include "common.h"
+#include "memory/cache.h"
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "burst.h"
 
 
 void init_cache(){
@@ -15,4 +19,36 @@ void init_cache(){
         cache2[i].dirty = 0;
     }
     test_time = 0;
+}
+
+int read_cache1(hwaddr_t address){
+    uint32_t group_id = ((address>>Cache_L1_Block_Bit)^(0x0380));//mask
+    uint32_t tag_id = (address>>(Cache_L1_Block_Bit+Cache_L1_Group_Bit));
+
+    uint32_t i;
+    uint32_t group_position = group_id*Cache_L1_Way_Size;
+
+    for(i=group_position; i<Cache_L1_Way_Size; i++){
+        if(cache1[i].tag==tag_id && cache1[i].valid==1){
+            //HIT Cache_1
+            return i;
+        }
+    }
+
+    //If fail to hit cache , replace with random algorithm
+    srand((unsigned int)(time(NULL)));
+    i = group_position + rand()%Cache_L1_Way_Size;
+
+    //read address from cache 1
+    int replace = read_cache2(address);
+    memcpy(cache1[i].data,cache2[replace].data,Cache_L1_Block_Size);
+
+    cache1[i].valid = 1;
+    cache2[i].tag = tag_id;
+    
+    return i;
+}
+
+int read_cache2(hwaddr_t address){
+    return -1;
 }
