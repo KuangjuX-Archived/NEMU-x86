7c37e842b39d247e28023f571045c16a61c7d463
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-12 00:10:23 +0800

    >  compile NEMU 3019244189 qcx Linux qcx-virtual-machine 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:10:23 up  1:17,  1 user,  load average: 0.48, 0.32, 0.18 962b7a861195ec72b86157175de0aa39af466526

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -8,6 +8,8 @@
 void public_ddr3_read(hwaddr_t addr, void* data);
 void public_ddr3_write(hwaddr_t addr, void* data, uint8_t *mask);
 
+void dram_write(hwaddr_t addr, size_t len, uint32_t data);
+
 void init_cache(){
     //initialize cache L1
     int i;
@@ -97,10 +99,60 @@ int read_cache2(hwaddr_t address){
     return i;
 }
 
-void write_cache1(hwaddr_t address, size_t len, uint32_t data){
+void write_cache1(hwaddr_t addr, size_t len, uint32_t data){
+    uint32_t group_idx = (addr >> Cache_L1_Block_Bit) & (Cache_L1_Group_Size - 1);
+    uint32_t tag = (addr >> (Cache_L1_Group_Bit + Cache_L1_Block_Bit));
+    uint32_t offset = addr & (Cache_L1_Block_Size - 1);
 
+    int i,group = group_idx * Cache_L1_Way_Size;
+    for (i = group + 0;i < group + Cache_L1_Way_Size;i ++){
+        if (cache1[i].valid == 1 && cache1[i].tag == tag){// WRITE HIT
+            /*write through*/
+            if (offset + len > Cache_L1_Block_Size){
+                dram_write(addr,Cache_L1_Block_Size - offset,data);
+                memcpy(cache1[i].data + offset, &data, Cache_L1_Block_Size - offset);
+                /*Update Cache2*/
+                write_cache2(addr,Cache_L1_Block_Size - offset,data);
+
+                write_cache1(addr + Cache_L1_Block_Size - offset,len - (Cache_L1_Block_Size - offset),data >> (Cache_L1_Block_Size - offset));
+            }else {
+                dram_write(addr,len,data);
+                memcpy(cache1[i].data + offset, &data, len);
+                /*Update Cache2*/
+                write_cache2(addr,len,data);
+            }
+            return;
+        }
+    }
+    // /*not write allocate*/
+    // PA3 task1
+    // dram_write(addr,len,data);
+
+    // PA3 optional task1
+    write_cache2(addr,len,data);
 }
 
-void write_cache2(hwaddr_t address, size_t len, uint32_t data){
-    
+void write_cache2(hwaddr_t addr, size_t len, uint32_t data){
+    uint32_t group_idx = (addr >> Cache_L2_Block_Bit) & (Cache_L2_Group_Size - 1);
+    uint32_t tag = (addr >> (Cache_L2_Group_Bit + Cache_L2_Block_Bit));
+    uint32_t offset = addr & (Cache_L2_Block_Size - 1);
+
+    int i,group = group_idx * Cache_L2_Way_Size;
+    for (i = group + 0;i < group + Cache_L2_Way_Size;i ++){
+        if (cache2[i].valid == 1 && cache2[i].tag == tag){// WRITE HIT
+            cache2[i].dirty = 1;
+            if (offset + len > Cache_L2_Block_Size){
+                memcpy(cache2[i].data + offset, &data, Cache_L2_Block_Size - offset);
+                write_cache2(addr + Cache_L2_Block_Size - offset,len - (Cache_L2_Block_Size - offset),data >> (Cache_L2_Block_Size - offset));
+            }else {
+                memcpy(cache2[i].data + offset, &data, len);
+            }
+            return;
+        }
+    }
+    /*write allocate*/
+    i = read_cache2(addr);
+    cache2[i].dirty = 1;
+    memcpy(cache2[i].data + offset,&data,len);
 }
+
