59e791f3807a0f42c442f099dc91b93ffceb58a9
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-13 14:59:53 +0800

    >  compile NEMU 3019244189 qcx Linux qcx-virtual-machine 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:59:53 up  1:01,  1 user,  load average: 0.62, 0.28, 0.17 b9ae2ea212ee103493b3e2889a8bb096847267ea

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -33,11 +33,12 @@ int read_cache1(hwaddr_t address){
     uint32_t group_id = (address >> Cache_L1_Block_Bit) & (Cache_L1_Block_Size-1);//mask
     uint32_t tag_id = (address >> (Cache_L1_Block_Bit + Cache_L1_Group_Bit));
 
-    int i, group_position = group_id * Cache_L1_Way_Size;
+    int i, group_position;
+    group_position = group_id * Cache_L1_Way_Size;
     
     
-    for(i = group_position; i < group_position + Cache_L1_Way_Size; i++){
-        if(cache1[i].tag==tag_id && cache1[i].valid==1){
+    for(i = group_position; i < (group_position + Cache_L1_Way_Size); i++){
+        if(cache1[i].valid == 1 && cache1[i].tag == tag_id){
             //HIT Cache_1
 
 #ifndef TEST
