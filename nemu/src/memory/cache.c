00029d15747c0d94e0c758dc7ee6f561861c4a41
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-13 15:10:42 +0800

    >  compile NEMU 3019244189 qcx Linux qcx-virtual-machine 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:10:42 up  1:12,  1 user,  load average: 0.32, 0.23, 0.19 58b7924dae066b8387597724b605f9635f53a877

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -69,36 +69,6 @@ int read_cache1(hwaddr_t address){
 
 }
 
-// int read_cache1(hwaddr_t addr){
-//     uint32_t group_idx = (addr >> Cache_L1_Block_Bit) & (Cache_L1_Group_Size - 1);
-//     uint32_t tag = (addr >> (Cache_L1_Group_Bit + Cache_L1_Block_Bit));
-//     // uint32_t block_start = (addr >> Cache_L1_Block_Bit) << Cache_L1_Block_Bit;
-
-//     int i,group = group_idx * Cache_L1_Way_Size;
-//     for (i = group + 0;i < group + Cache_L1_Way_Size;i ++){
-//         if (cache1[i].valid == 1 && cache1[i].tag == tag){// READ HIT
-
-// #ifdef Test
-//             test_time += 2;//HIT in Cache1
-// #endif
-//             return i;
-//         }
-//     }
-    
-    // Find in Cache2
-    // int pl = read_cache2(addr);   
-    // srand(time(0));
-    // i = group + rand() % Cache_L1_Way_Size;
-    // memcpy(cache1[i].data,cache2[pl].data,Cache_L1_Block_Size);
-
-    // cache1[i].valid = 1;
-    // cache1[i].tag = tag;
-    // return i;
-// }
-
-
-
-
 
 int read_cache2(hwaddr_t address){
     uint32_t group_id = (address >> Cache_L2_Block_Bit) & (Cache_L2_Group_Size-1);
