f56d86d53d27eaf0905cf099ba2abaee0ca32048
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-28 21:01:08 +0800

    >  compile NEMU 3019244189 qcx Linux qcx-virtual-machine 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:01:08 up 1 day, 22:37,  1 user,  load average: 0.38, 0.26, 0.16 b91fa2416a9e49ef4b840fa557ac4564c3b09cd

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "memory/cache.h"
+#include "memory/tlb.h"
 #include "burst.h"
 #include <string.h>
 #include "nemu.h"
@@ -9,7 +10,35 @@ uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
 hwaddr_t page_translate(lnaddr_t addr){
-	return addr;
+	if (cpu.CR0.protect_enable == 1 && cpu.CR0.paging == 1){
+		//printf("%x\n",addr);
+		uint32_t dir = addr >> 22;
+		uint32_t page = (addr >> 12) & 0x3ff;
+		uint32_t offset = addr & 0xfff;
+		//read TLB
+		int i = read_tlb(addr);
+		if (i != -1) return (tlb[i].page_num << 12) + offset;
+		// get dir position
+		uint32_t dir_start = cpu.CR3.page_directory_base;
+		uint32_t dir_pos = (dir_start << 12) + (dir << 2);
+		PageDescriptor first_content;
+		first_content.val = hwaddr_read(dir_pos,4);
+		//printf("dir:%x,con:%x\n",dir_pos,first_content.val);
+		Assert(first_content.p == 1,"Dir Cannot Be Used!");
+		// get page position
+		uint32_t page_start = first_content.addr;
+		uint32_t page_pos = (page_start << 12) + (page << 2);
+		PageDescriptor second_content;
+		second_content.val =  hwaddr_read(page_pos,4);
+		Assert(second_content.p == 1,"Page Cannot Be Used!");
+		// get hwaddr
+		uint32_t addr_start = second_content.addr;
+		hwaddr_t hwaddr = (addr_start << 12) + offset;
+		write_tlb(addr,hwaddr);
+		return hwaddr;
+	}else {
+		return addr;
+	}
 }
 
 /* Memory accessing interfaces */
